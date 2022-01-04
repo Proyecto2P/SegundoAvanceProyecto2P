ddfcdc7be8d0e506bf73372268cc9199ebfdb550
cruzmateo163 <cruzmateo163@gmail.com>
2022-01-03 20:44:21 -0500

    Se agrega archivos segundo avance

---
+++ ProyectoTuristeando/ProyectoTuristeando/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Data.SqlClient;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace ProyectoTuristeando
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        // Connection string for using Windows Authentication.
+        private string connectionString =
+            @"Data Source=DESKTOP-BO99RMD;Initial Catalog=Turismo;Integrated Security=SSPI";
+
+        // This is an example connection string for using SQL Server Authentication.
+        // private string connectionString =
+        //     @"Data Source=YourServerName\YourInstanceName;Initial Catalog=DatabaseName; User Id=XXXXX; Password=XXXXX";
+
+        public string ConnectionString { get => connectionString; set => connectionString = value; }
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            
+
+        }
+    }
+}
